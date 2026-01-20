37bc24fc394d8602bf342030b34a9277f1211b97
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-01-20 07:39:28 -0800

    Update System Restore - Detect.ps1

--- System Restore - Detect.ps1
+++ System Restore - Detect.ps1
@@ -6,13 +6,14 @@ try {
     $currentValue = Get-ItemProperty -Path $registryPath -Name $registryKey -ErrorAction Stop
 
     if ($currentValue.$registryKey -eq $desiredValue) {
-        Write-Host "RPSessionInterval is set to $desiredValue/Script will exit"
+        Write-Output "RPSessionInterval is set to $desiredValue/Script will exit"
         Exit 0
     } else {
-        Write-Host "System Protection not enabled."
+        Write-Output "System Protection not enabled."
         Exit 1
     }
 } catch {
-    Write-Host "Error accessing registry key: $($_.Exception.Message)"
+    Write-Output "Error accessing registry key: $($_.Exception.Message)"
     Exit 1
+
 }
