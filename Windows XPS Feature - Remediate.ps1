19fc39ab04120ab6b4e3d1c25122b6a14790a3f9
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2026-01-20 07:39:02 -0800

    Update Windows XPS Feature - Remediate.ps1

--- Windows XPS Feature - Remediate.ps1
+++ Windows XPS Feature - Remediate.ps1
@@ -1,11 +1,12 @@
 try {
     $Feature = Enable-WindowsOptionalFeature -Online -FeatureName "Printing-XPSServices-Features"
     if ($Feature.state -ne "Enabled") {
-        Write-Host "Feature failed to enable"
+        Write-Output "Feature failed to enable"
         Exit 0
     }
 }
 catch {
-    Write-Warning "Not Enabled"
+    Write-Output "Not Enabled"
     Exit 1
 }
+
